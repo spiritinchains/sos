0beb6dccd6e2cbea2fed48aad0b0b6e91014280b
Jawad Kabir <smjawadkabir@gmail.com>
2025-10-22 02:24:52 +0600

    Remove timeout and framebuffer code

--- kernel/common/main.c
+++ kernel/common/main.c
@@ -48,21 +48,6 @@ void kmain(void) {
         hcf();
     }
 
-    // Ensure we got a framebuffer.
-    if (framebuffer_request.response == NULL
-     || framebuffer_request.response->framebuffer_count < 1) {
-        hcf();
-    }
-
-    // Fetch the first framebuffer.
-    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
-
-    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
-    for (size_t i = 0; i < 100; i++) {
-        volatile uint32_t *fb_ptr = framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
-    }
-
     serial_write(0, (uint8_t *) "Hello from SOS", 14);
 
     // We're done, just hang...
