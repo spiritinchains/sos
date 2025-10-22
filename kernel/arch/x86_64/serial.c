b021f38139ebc9e530faa5b412bc066aaf05c19c
Jawad Kabir <smjawadkabir@gmail.com>
2025-10-22 12:05:25 +0600

    Implement Serial Write

--- kernel/arch/x86_64/serial.c
+++ kernel/arch/x86_64/serial.c
@@ -1,6 +1,27 @@
 #include "serial.h"
 #include "common.h"
 
+#include <stdbool.h>
+
+static const uint16_t PORT[] = {
+    0x3f8,
+    0x2f8
+};
+
+#define IS_PORT_ID_VALID(id) (((id) >= 0) && ((id) < (int) (sizeof(PORT) / sizeof(uint16_t))))
+
+static bool serial_is_transmit_empty(const uint16_t address)
+{
+    uint16_t value;
+    inb(address + SERIAL_REG_READ_LINE_STATUS, value);
+    return (bool) (value & SERIAL_LINE_THRE);
+}
+
+static void serial_transmit_byte(const uint16_t address, const uint8_t data)
+{
+    outb(address, data);
+}
+
 void serial_read(const int id, uint8_t data[], const size_t len)
 {
     // TODO
@@ -8,7 +29,16 @@ void serial_read(const int id, uint8_t data[], const size_t len)
 
 void serial_write(const int id, const uint8_t data[], const size_t len)
 {
-    // TODO
+    if (!IS_PORT_ID_VALID(id))
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < len; i++)
+    {
+        while (!serial_is_transmit_empty(PORT[id])) {}
+        serial_transmit_byte(PORT[id], data[i]);
+    }
 }
 
 void serial_set_baud(const int id, const serial_baud_t baud)
