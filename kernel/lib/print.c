d1627e63b8886894150cbfb8e301ff2bc124e9f2
Jawad Kabir <smjawadkabir@gmail.com>
2025-12-26 06:03:37 +0600

    Add stubs for print.c

---
+++ kernel/lib/print.c
@@ -0,0 +1,31 @@
+#include <print.h>
+
+int printf(const char *fmt, ...)
+{
+    return 0;
+}
+
+int sprintf(char *buf, const char *fmt, ...)
+{
+    return 0;
+}
+
+int snprintf(char *buf, size_t bufsize, const char *fmt, ...)
+{
+    return 0;
+}
+
+int vprintf(const char *fmt, va_list vlist)
+{
+    return 0;
+}
+
+int vsprintf(char *buf, const char *fmt, va_list vlist)
+{
+    return 0;
+}
+
+int vsnprintf(char *buf, size_t bufsize, const char *fmt, va_list vlist)
+{
+    return 0;
+}
